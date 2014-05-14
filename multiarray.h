4a1b260c9f28663a6d10b8d5847b4909457fe9df
Lierdakil <root@livid.pp.ru>
2014-05-14 18:37:08 +0400

    Remove debug asserts alltogether. Add check that operator() arg count=ndim

--- multiarray.h
+++ multiarray.h
@@ -15,35 +15,29 @@ private:
     idx_t size;
     T* data;
 
-#pragma push_macro("assert")
-#undef assert
-#define assert(x)
     inline idx_t index(smallidx_t stridesidx __attribute__((unused)), smallidx_t i) const {
-        assert(stridesidx==ndim-1);
         return i;
     }
 
     template<typename ... Types>
     inline idx_t index(smallidx_t stridesidx, smallidx_t i, Types... rest) const {
-        assert(stridesidx<ndim);
         return i*strides[stridesidx]+index(stridesidx+1,rest...);
     }
 
     inline idx_t fill_strides(smallidx_t stridesidx __attribute__((unused))) const {
-        assert(stridesidx==ndim-1);
         return 1;
     }
 
     template<typename ... Types>
     inline idx_t fill_strides(smallidx_t stridesidx, smallidx_t i, Types... rest) const {
-        assert(stridesidx<ndim-1);
         return strides[stridesidx]=fill_strides(stridesidx+1,rest...)*i;
     }
-#undef assert
-#pragma pop_macro("assert")
+
 public:
     template<typename ... Types>
-    MultiArray(smallidx_t nfirst, Types... counts) : strides(nullptr) {
+    MultiArray(smallidx_t nfirst, Types... counts) :
+        strides(nullptr)
+    {
         ndim=sizeof...(counts)+1;
         if(ndim>1) {
             strides=new idx_t[ndim-1];
@@ -61,6 +55,7 @@ public:
 
     template<typename ... Types>
     inline T& operator()(Types... indexes) const {
+        assert(sizeof...(indexes)==ndim);
         idx_t idx=index(0,indexes...);
         assert(idx<size);
         return data[idx];
