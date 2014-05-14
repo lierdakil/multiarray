d387c43074bbf35c2137e66684dda555b8358acd
Lierdakil <root@livid.pp.ru>
2014-05-14 18:18:41 +0400

    Remove non-essential asserts and some other optimizations

--- multiarray.h
+++ multiarray.h
@@ -15,9 +15,12 @@ private:
     idx_t size;
     T* data;
 
-    inline idx_t index(smallidx_t stridesidx) const {
-        assert(stridesidx==ndim);
-        return 0;
+#pragma push_macro("assert")
+#undef assert
+#define assert(x)
+    inline idx_t index(smallidx_t stridesidx __attribute__((unused)), smallidx_t i) const {
+        assert(stridesidx==ndim-1);
+        return i;
     }
 
     template<typename ... Types>
@@ -26,24 +29,28 @@ private:
         return i*strides[stridesidx]+index(stridesidx+1,rest...);
     }
 
-    inline idx_t fill_strides(smallidx_t stridesidx) const {
+    inline idx_t fill_strides(smallidx_t stridesidx __attribute__((unused))) const {
         assert(stridesidx==ndim-1);
-        strides[stridesidx]=1;
         return 1;
     }
 
     template<typename ... Types>
     inline idx_t fill_strides(smallidx_t stridesidx, smallidx_t i, Types... rest) const {
-        assert(stridesidx<ndim);
+        assert(stridesidx<ndim-1);
         return strides[stridesidx]=fill_strides(stridesidx+1,rest...)*i;
     }
+#undef assert
+#pragma pop_macro("assert")
 public:
     template<typename ... Types>
-    MultiArray(smallidx_t nfirst, Types... counts) {
+    MultiArray(smallidx_t nfirst, Types... counts) : strides(nullptr) {
         ndim=sizeof...(counts)+1;
-        strides=new idx_t[ndim];
-        fill_strides(0,counts...);
-        size=nfirst*strides[0];
+        if(ndim>1) {
+            strides=new idx_t[ndim-1];
+            fill_strides(0,counts...);
+            size=nfirst*strides[0];
+        } else
+            size=nfirst;
         data=new T[size];
     }
 
